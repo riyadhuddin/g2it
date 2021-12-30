bdedcab7aba61ebe9517ffbf864cd98cf8d91bad
Riyadh Uddin <noreplythnx@gmail.com>
2021-12-30 14:56:02 +0600

    :p

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+module.exports = (robot) => {
+  robot.on("issues.opened", async (context) => {
+    const { body } = context.payload.issue;
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+    // create a comment
+    const comment = context.issue({
+      body: body.includes("Thanks") ? "You are Welcome!" : "Thanks!",
     });
-    return context.octokit.issues.createComment(issueComment);
+    // publish it
+    return context.github.issues.createComment(comment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
